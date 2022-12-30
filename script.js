5c90d37bd1e476e00b54f4d8ec9577c0b1713dcd
Ben <stealthton@gmail.com>
2022-12-30 10:14:15 +0000

    initial player and dealer scores for turn 1 printing to console

--- script.js
+++ script.js
@@ -12,10 +12,12 @@
 //if dealer >21 --> lose
 //if stay --> do compare hand()
 //
-var player = [0]; // keep track of every card in the array then add them to get scores
-var dealer = [0];
+var player = []; // keep track of every card in the array then add them to get scores
+var dealer = [];
 var player_total = 0; //keep added score in here
 var dealer_total = 0;
+var player_score = 0; //keep added score in here
+var dealer_score = 0;
 
 //prompt user to play game - if play = true start game otherwise reload prompt
 function UserStart() {
@@ -23,41 +25,61 @@ function UserStart() {
   if (confirm(play) == true) {
     StartGame();
   } else {
-    // prompt("You canceled!");
+    // re-ask the question "Would you like to play Blackjack?" to basically wait until someone wants to play
     UserStart();
   }
 }
 
 function StartGame() {
   console.log("You started the game");
-  Deal();
+  if (player.length === 0) {
+    player_score = Deal();
+    dealer_score = Deal();
+    console.log("Player Initil Score : " + player_score);
+    console.log("Dealer Initil Score : " + dealer_score);
+  }
+  else {
+    player_score = Twist();
+    dealer_score = Twist();
+    console.log("Player total Score : " + player_total);
+    console.log("Dealer total Score : " + dealer_total);
+  }
 }
 //function to deal cards- may split into payer and dealer later
 function Deal() {
   console.log("Initial Deal...");
-  let player_score = Twist(2, 12);//need a condition here to swap between 2-21(2 cards initial deal) 4-11(twist)
-  player.push(player_score);//add a random score to the player array
-  console.log("player score : " + player_score);//keep last score in here
+  let min = 2;
+  let max = 22;
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  // let player_score = Twist(2, 12);//need a condition here to swap between 2-21(2 cards initial deal) 4-11(twist)
+  // player.push(player_score);//add a random score to the player array
+  // console.log("player score : " + player_score);//keep last score in here
+  let score = Math.floor(Math.random() * (max - min) + min); // The maximum is exclusive and the minimum is inclusive
+  // score = Math.floor(Math.random() * (max - min) + min); // The maximum is exclusive and the minimum is inclusive
+  return score;
 }
 //function to generate a twist
-function Twist(min, max) {
+function Twist() {
+  let min = 4;
+  let max = 12;
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min) + min); // The maximum is exclusive and the minimum is inclusive
 }
 //add up player scores
-function PlayerTotal(){
-  for(let i = 0; i < player.length; i++){
+function PlayerTotal() {
+  for (let i = 0; i < player.length; i++) {
     player_total += player[i];
   }
   return player_total;
 }
 //add up dealer scores
-function DealerTotal(){
-  for(let i = 0; i < player.length; i++){
-    player_total += player[i];
+function DealerTotal() {
+  for (let i = 0; i < dealer.length; i++) {
+    dealer_total += dealer[i];
   }
-  return player_total;
+  return dealer_total;
 }
 
 
