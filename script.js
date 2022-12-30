884cd55856d482cb78613c83e4c0a1d2b2c38c91
Ben <stealthton@gmail.com>
2022-12-30 09:06:38 +0000

    start stop game behaviour working at user end

--- script.js
+++ script.js
@@ -16,20 +16,26 @@
 let player = [0]; // keep track of every card in the array then add them to get scores
 let dealer = [0];
 
-//prompt user to play game - if play = yes start game otherwise reload prompt
-let play = "Would you like to play Blackjack ? \nEither OK or Cancel.";
-if (confirm(play) == true) {
-  Start_Game();
-} else {
-  prompt("You canceled!");
+//prompt user to play game - if play = true start game otherwise reload prompt
+function User_Start() {
+  let play = "Would you like to play Blackjack ? \nEither OK or Cancel.";
+  if (confirm(play) == true) {
+    Start_Game();
+  } else {
+    prompt("You canceled!");
+    User_Start();
+  }
 }
 
-
 function Start_Game() {
-    console.log("You started the game");
-    Deal();
+  console.log("You started the game");
+  Deal();
 }
 
 function Deal() {
-    console.log("Dealing");
+  console.log("Dealing");
+  player.push = Math.random(22);
 }
+
+//running above functions to test behaviour
+User_Start();
